774a22a52f48a43bb3b722de72d300376dc2dd8c
Jonathan Puckey <jonathan@studiomoniker.com>
2017-03-28 13:47:50 +0200

    remove unused

--- src/utils/feature.js
+++
@@ -1,7 +0,0 @@
-import { getUrlParameter } from './url';
-
-export const isMobile = /android|iPad|webOS|iPhone|iPod|IEMobile/i
-    .test(navigator.userAgent);
-
-export const isApp = /crosswalk/i
-    .test(navigator.userAgent) || !!getUrlParameter('app');
--- src/utils/hostname.js
+++
@@ -1,5 +0,0 @@
-export default function getHostName(url) {
-  return url
-    .split('/')[url.indexOf('://') > -1 ? 2 : 0]
-    .replace(/(www\.)*/g, '');
-}
