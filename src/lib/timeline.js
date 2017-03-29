d5cf7650755d383a73f4344ba06d9a40f6d255da
David van Gelder de Neufville <_@neufv.website>
2017-03-29 15:04:23 +0200

    fix bugs, check if callback exists

--- src/lib/timeline.js
+++ src/lib/timeline.js
@@ -8,13 +8,19 @@ function add(time, marker, callback = false) {
 }
 
 function tick(time) {
-  for (let i = 0; i <= timeline.length; i++) {
+  for (let i = 0; i < timeline.length; i++) {
     const timelineEvent = timeline[i];
 
     if (time > timelineEvent.time) {
+      // if there are no more events or there are and this one has not expired yet
       if (!timeline[i + 1] || (timeline[i + 1] && time <= timeline[i + 1].time)) {
+        // emit the marker as an event
         emitter.emit(timelineEvent.marker);
-        timelineEvent.callback();
+
+        // callback if one exists
+        if (timelineEvent.callback) {
+          timelineEvent.callback();
+        }
       }
     }
   }
