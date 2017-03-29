4aa561764621ebc8399c854db215e1487f8070f1
Jonathan Puckey <jonathan@studiomoniker.com>
2017-03-29 16:30:34 +0200

    fly through space

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import 'babel-polyfill';
 import './index.scss';
 import viewer from './viewer';
 import props from './props';
+import settings from './settings';
 
 props.prepare(() => {
+  const { holeHeight, depth } = settings.space;
+  viewer.camera.position.y = holeHeight;
+  props.space.position.y = settings.space.height * 0.5;
   viewer.scene.add(props.space);
+  const then = Date.now();
+  viewer.events.on('tick', () => {
+    const loopLength = 8000;
+    const time = (Date.now() - then) % loopLength;
+    const ratio = time / loopLength;
+    viewer.camera.position.z = -1 * ((ratio * depth) - (depth / 2));
+  });
 });
---
+++ src/settings.js
@@ -0,0 +1,9 @@
+export default {
+  space: {
+    depth: 6,
+    height: 4,
+    offset: -0.2 * 6,
+    width: 4,
+    holeHeight: 1.6,
+  },
+};
