c445989a26c3a2a88da144007233fe0a0071c91f
Jonathan Puckey <jonathan@studiomoniker.com>
2017-03-29 17:08:19 +0200

    avoid console.log

--- src/lib/OBJLoader.js
+++ src/lib/OBJLoader.js
@@ -422,7 +422,7 @@ module.exports = function( THREE ){
 
 		parse: function ( text ) {
 
-			console.time( 'OBJLoader' );
+			// console.time( 'OBJLoader' );
 
 			var state = this._createParserState();
 
@@ -736,7 +736,7 @@ module.exports = function( THREE ){
 
 			}
 
-			console.timeEnd( 'OBJLoader' );
+			// console.timeEnd( 'OBJLoader' );
 
 			return container;
 
