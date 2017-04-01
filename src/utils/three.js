b1c2c2d34bfc883949f6370782a98e086ae01d27
Jonathan Puckey <jonathan@studiomoniker.com>
2017-04-01 18:52:35 +0200

    tempColor

--- src/utils/three.js
+++ src/utils/three.js
@@ -2,10 +2,13 @@ import * as THREE from '../lib/three';
 
 const VECTOR = new THREE.Vector3();
 const QUATERNION = new THREE.Quaternion();
+const COLOR = new THREE.Color();
 
 export const tempVector = (x = 0, y = 0, z = 0) => VECTOR.set(x, y, z);
 
 export const tempQuaternion = (x = 0, y = 0, z = 0, w = 0) => QUATERNION.set(x, y, z, w);
+
+export const tempColor = (r = 0, g = 0, b = 0) => COLOR.set(r, g, b);
 
 export const createInstancedMesh = (num, color, geometry) => {
   const material = new THREE.MeshLambertMaterial({ color });
