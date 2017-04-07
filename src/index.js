fd8bc5a157e87882d0d9d315244730c564a7228b
Jonathan Puckey <jonathan@studiomoniker.com>
2017-04-07 16:23:41 +0200

    use audio

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import settings from './settings';
 import Room from './room';
 import * as THREE from './lib/three';
 import storage from './storage';
-import timeline from './lib/timeline';
 import Orb from './orb';
-import audio from './lib/audioEngine';
+import audio from './audio';
 import audioSrc from './public/sound/lcd-14loops.mp3';
 
 window.THREE = THREE;
@@ -18,27 +17,31 @@ window.THREE = THREE;
 props.prepare(() => {
   storage.loadPlaylist('curated', (error, playlist) => {
     if (error) throw error;
-    const { loopLength, roomDepth, roomOffset } = settings;
+    audio.load({ src: audioSrc, loops: 14 }, (loadError) => {
+      if (loadError) throw loadError;
 
-    const orb = new Orb();
+      audio.play();
 
-    const rooms = playlist.map(url => new Room(url, {
-      showHead: !/head\=false/.test(url)
-    }));
+      const orb = new Orb();
 
-    eachLimit(rooms, 4, (room, callback) => room.load(callback), (loadError) => {
-      if (error) throw loadError;
-      // Done loading rooms
-    });
+      const rooms = playlist.map(url => new Room(url, {
+        showHead: !/head\=false/.test(url),
+      }));
 
-    const then = Date.now();
-    viewer.events.on('tick', () => {
-      const time = Date.now() - then;
-      const offset = 0.5;
-      const ratio = (time / loopLength) - offset;
-      viewer.camera.position.z = (ratio * roomDepth) + roomOffset;
-      orb.move(viewer.camera.position.z);
-      rooms.forEach(room => room.gotoTime(time));
+      eachLimit(rooms, 4, (room, callback) => room.load(callback), (roomLoadError) => {
+        if (roomLoadError) throw roomLoadError;
+        // Done loading rooms
+      });
+
+      const { roomDepth, roomOffset } = settings;
+      const tick = () => {
+        audio.tick();
+        viewer.camera.position.z = ((audio.progress - 0.5) * roomDepth) + roomOffset;
+        orb.move(viewer.camera.position.z);
+        rooms.forEach(room => room.gotoTime(audio.time * 1000));
+      };
+
+      viewer.events.on('tick', tick);
     });
   });
 });
