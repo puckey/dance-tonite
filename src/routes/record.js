01c3c33a7f6646dcb192f348a39924a9456ba597
Jonathan Puckey <jonathan@studiomoniker.com>
2017-04-10 18:22:20 +0200

    add initial recording mode

--- src/routes/record.js
+++ src/routes/record.js
@@ -1,3 +1,13 @@
+import Room from '../room';
+import Orb from '../orb';
+import audio from '../audio';
+import audioSrc from '../public/sound/lcd-loop.ogg';
+import viewer from '../viewer';
+import { roomDepth, roomOffset } from '../settings';
+
+let room;
+let orb;
+let tick;
 
 export default {
   hud: {
@@ -5,10 +15,31 @@ export default {
   },
 
   mount: () => {
+    viewer.camera.position.z = 0;
+    viewer.switchCamera('default');
+    room = new Room();
+    orb = new Orb();
 
+    audio.load({
+      src: audioSrc,
+      loops: 2,
+    }, (loadError) => {
+      if (loadError) throw loadError;
+      audio.play();
+    });
+
+    tick = () => {
+      audio.tick();
+      const z = (audio.progress * roomDepth) + roomOffset;
+      orb.move(z);
+    };
+
+    viewer.events.on('tick', tick);
   },
 
   unmount: () => {
-
+    room.destroy();
+    orb.destroy();
+    viewer.events.off('tick', tick);
   },
 };
