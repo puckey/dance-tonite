9457b71f4672687dc6b11792b0f5a61c0a336be4
puckey <jonathan@puckey.studio>
2017-04-26 21:49:34 +0200

    load playlist async

--- src/storage.js
+++ src/storage.js
@@ -3,19 +3,17 @@ import fetch from 'unfetch';
 
 const uploader = new UniqueS3Uploader('https://ymm-recorder.puckey.studio/new/');
 
-const persist = (json) => {
-  return new Promise((resolve, reject) => {
-    uploader.upload(json, (error, data) => {
-      if (error) return reject(error);
-      resolve(data.uri);
-    });
+const persist = (json) => new Promise((resolve, reject) => {
+  uploader.upload(json, (error, data) => {
+    if (error) return reject(error);
+    resolve(data.uri);
   });
-};
+});
 
-const loadPlaylist = async (filename, callback) => {
+const loadPlaylist = async (filename) => {
   const response = await fetch(`public/playlists/${filename}`);
   const data = await response.json();
-  callback(null, data);
+  return data;
 };
 
 export default {
