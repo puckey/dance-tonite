02f26d337b466f4e5e2ec2486fa58d2d6b18041d
David van Gelder de Neufville <_@neufv.website>
2017-05-03 10:18:46 +0200

    add room and basic timelines

--- src/routes/record/tutorial.js
+++ src/routes/record/tutorial.js
@@ -6,15 +6,46 @@ import viewer from '../../viewer';
 import settings from '../../settings';
 import createTimeline from '../../lib/timeline';
 import { waitRoomColor, recordRoomColor } from '../../theme/colors';
+import { Vector3 as Vector } from '../../lib/three';
 
 const TUTORIAL_RECORDING_URL = '1033470119233-6feddefd.json';
 
 const { roomDepth, roomOffset } = settings;
 
+const tutorialText = h('div.tutorial-text', '');
+const skipTutorialButton = h('div.skip-tutorial-button', 'Skip Tutorial');
+const line = h('div.line');
+
+let windowWidth;
+let windowHeight;
+let lineOriginX;
+let lineOriginY;
+
 const getLineTransformString = (x1, y1, x2, y2) => {
   const length = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
   const angle = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
-  return `rotate(${angle}) scaleX(${length / 100}) translate(${x1}, ${y1})`;
+  return `translate(${x1}px, ${y1}px) rotate(${angle}deg) scaleX(${length / 100})`;
+};
+
+const updateWindowDimensions = () => {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+  lineOriginX = windowWidth / 2;
+  lineOriginY = tutorialText.offsetHeight;
+};
+
+const get2DCoordinates = position => {
+  const vector = new Vector();
+  vector.copy(position);
+
+  // map to normalized device coordinate (NDC) space
+  vector.project(viewer.camera);
+
+  // map to 2D screen space
+  const x = Math.round((vector.x + 1) * windowWidth / 2);
+  const y = Math.round((-vector.y + 1) * windowHeight / 2);
+
+  return { x, y };
 };
 
 export default async (goto) => {
@@ -87,23 +118,21 @@ export default async (goto) => {
     const z = (progress - 0.5) * -roomDepth - roomOffset;
     orb.move(z);
     orb2.move(z - roomDepth * 2);
-  };
 
-  window.onResize = () => {
-    tutorialTextHeight = tutorialText.height;
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
+    const { x, y } = get2DCoordinates(orb.mesh.position);
+    line.style.transform = getLineTransformString(
+      x,
+      y,
+      lineOriginX,
+      lineOriginY
+    );
   };
-
-  const tutorialText = h('div.tutorial-text', '');
-  const skipTutorialButton = h('div.skip-tutorial-button', 'Skip Tutorial');
 
   document.querySelector('.hud').appendChild(tutorialText);
   document.querySelector('.hud').appendChild(skipTutorialButton);
+  document.querySelector('.hud').appendChild(line);
 
-  let tutorialTextHeight = tutorialText.height;
-  let windowWidth = window.innerWidth;
-  let windowHeight = window.innerHeight;
+  skipTutorialButton.classList.remove('mod-hidden');
 
   await audio.load({
     src: '/public/sound/room-7.ogg',
@@ -111,7 +140,7 @@ export default async (goto) => {
   });
 
   viewer.switchCamera('orthographic');
-  viewer.camera.position.z = 1;
+  viewer.camera.position.z = 1.3;
   viewer.camera.position.y = 2;
   viewer.camera.zoom = 0.7;
   viewer.camera.updateProjectionMatrix();
@@ -133,11 +162,15 @@ export default async (goto) => {
   audio.fadeIn();
   viewer.events.on('tick', tick);
   skipTutorialButton.addEventListener('click', performSkip);
+  window.addEventListener('resize', updateWindowDimensions);
+  updateWindowDimensions();
 
   return () => {
+    window.removeEventListener('resize', updateWindowDimensions);
     skipTutorialButton.removeEventListener('click', performSkip);
     document.querySelector('.hud').removeChild(tutorialText);
     document.querySelector('.hud').removeChild(skipTutorialButton);
+    document.querySelector('.hud').removeChild(line);
     audio.reset();
     Room.reset();
     audio.fadeOut();
