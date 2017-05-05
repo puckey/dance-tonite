1ac7d833b29bf22b554c1026ee7f7a4cf37f7e48
Stewart <stew@rtsmith.io>
2017-05-05 15:52:36 -0400

    Linted controllers.js as proof of ESLint.

--- src/controllers.js
+++ src/controllers.js
@@ -9,19 +9,11 @@ let leftPress;
 let rightPress;
 
 
-
-
-//THREE.VRController.verbosity=1;
-//console.log( THREE.VRController.controllers )
-
-
-//console.log('can has window events?', window.addEventListener)
-
 window.addEventListener('vr controller connected', (event) => {
   const controller = event.detail;
   //  Can take this further and see if controller.gamepad.hand contains a "left" or "right" string.
   //  (It's blank half the time... very unreliable!)
-  const handedness = leftController.gamepad===undefined ? 'left' : 'right'
+  const handedness = leftController.gamepad === undefined ? 'left' : 'right';
   controller.standingMatrix = viewer.controls.getStandingMatrix();//  For 6DOF VR rigs.
   controller.head = viewer.cameras.default;//  For 3DOF VR rigs.
   viewer.scene.add(controller);
@@ -38,31 +30,29 @@ window.addEventListener('vr controller connected', (event) => {
   });
   hand.add(text);
   text.rotation.x = -Math.PI * 0.5;
-  if (handedness==='left') text.position.set(-0.12, 0, -0.022);
+  if (handedness === 'left') text.position.set(-0.12, 0, -0.022);
   else text.position.set(0.03, 0, -0.022);
 
-  controller.setButtonVisibility = ( flag ) => {
+  controller.setButtonVisibility = (flag) => {
     hand.getObjectByName('button').visible = flag;
-  }
+  };
 
-  function handlePress(){
-    if (handedness==='left' && leftPress) leftPress();
-    if (handedness==='right' && rightPress) rightPress();
-    //console.log('press event began',handedness)
+  function handlePress() {
+    if (handedness === 'left' && leftPress) leftPress();
+    if (handedness === 'right' && rightPress) rightPress();
   }
-  if (controller.style==='rift') {
+  if (controller.style === 'rift') {
     controller.addEventListener('primary press began', handlePress);
     controller.addEventListener('primary press ended', handlePress);
-  }
-  else {
+  } else {
     controller.addEventListener('thumbpad press began', handlePress);
     controller.addEventListener('thumbpad press ended', handlePress);
   }
 
-  controller.updateOn = ( options ) => {
+  controller.updateOn = (options) => {
     text.updateLabel(options.text);
     if (options.onPress) controller.setButtonVisibility(true);
-    if (handedness==='left'){
+    if (handedness === 'left') {
       leftPress = () => {
         if (options.removeOnPress) {
           text.updateLabel('');
@@ -71,8 +61,7 @@ window.addEventListener('vr controller connected', (event) => {
         }
         if (options.onPress) options.onPress();
       };
-    }
-    else if(handedness==='right'){
+    } else if (handedness === 'right') {
       rightPress = () => {
         if (options.removeOnPress) {
           text.updateLabel('');
@@ -83,23 +72,23 @@ window.addEventListener('vr controller connected', (event) => {
       };
     }
   };
-  controller.updateOff = ( options ) => {
+  controller.updateOff = () => {
     text.updateLabel('');
-    if (handedness==='left') leftPress = null;
-    else if(handedness==='right') rightPress = null;
+    if (handedness === 'left') leftPress = null;
+    else if (handedness === 'right') rightPress = null;
     controller.setButtonVisibility(false);
   };
 
 
-  controller.addEventListener('disconnected', ( event )=>{
-    controller.visible=false;
+  controller.addEventListener('disconnected', () => {
+    controller.visible = false;
   });
 
 
   //  Export as either left or right.
 
-  if (handedness==='left') leftController = controller;
-  else if (handedness==='right') rightController = controller;
+  if (handedness === 'left') leftController = controller;
+  else if (handedness === 'right') rightController = controller;
 
 
   //  Oh, and we ought to patch viewer{} as well.
@@ -107,37 +96,34 @@ window.addEventListener('vr controller connected', (event) => {
 
   viewer.controllers[0] = leftController;
   viewer.controllers[1] = rightController;
-
 }, false);
-
-
 
 
 export default {
   update({ left, right } = {}) {
-    if(leftController.gamepad){//  Did we find this controller?
+    if (leftController.gamepad) { //  Did we find this controller?
       if (left) leftController.updateOn(left);
       else leftController.updateOff(left);
     }
-    if(rightController.gamepad){//  Did we find this controller?
+    if (rightController.gamepad) { //  Did we find this controller?
       if (right) rightController.updateOn(right);
       else rightController.updateOff(right);
     }
   },
 
   add() {
-    leftController.visible  = true;
+    leftController.visible = true;
     rightController.visible = true;
   },
 
   remove() {
-    rightPress = leftPress  = null;
-    leftController.visible  = false;
+    rightPress = leftPress = null;
+    leftController.visible = false;
     rightController.visible = false;
   },
 
-  setButtonVisibility( hand, flag ){//  Quick hack...
-    if(hand==='left') leftController.setButtonVisibility(flag)
-    else if(hand==='right') rightController.setButtonVisibility(flag)
+  setButtonVisibility(hand, flag) { //  Quick hack...
+    if (hand === 'left') leftController.setButtonVisibility(flag);
+    else if (hand === 'right') rightController.setButtonVisibility(flag);
   },
 };
