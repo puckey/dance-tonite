2f38aa288090bdc44fdfc06393c5c37d746a6a06
puckey <jonathan@puckey.studio>
2017-06-05 18:12:22 +0200

    reset orthographic distance

--- src/viewer.js
+++ src/viewer.js
@@ -18,7 +18,7 @@ require('./lib/VRControls')(THREE);
 require('./lib/ViveController')(THREE);
 
 const events = emitter();
-const orthographicDistance = 8;
+const orthographicDistance = 4;
 
 const cameras = (function () {
   const { aspectRatio } = windowSize;
