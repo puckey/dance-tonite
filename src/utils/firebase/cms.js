b70f7b449a9662cbe5b401f26e6c87b00db24a8b
Jeff Nusz <jnusz@google.com>
2017-06-07 16:16:17 -0700

    sample cms function

--- src/utils/firebase/cms.js
+++ src/utils/firebase/cms.js
@@ -1,1 +1,17 @@
+// import cms from './utils/firebase/cms';
+//
+// cms.getUnmoderatedRecordings().then( (data) => { console.log("data!", data)});
+
 import firebaseConnection from './connection';
+
+const getUnmoderatedRecordingsURL = 'https://us-central1-you-move-me.cloudfunctions.net/getUnmoderatedRecordings';
+
+const getUnmoderatedRecordings = () =>
+      firebaseConnection.contactServer(getUnmoderatedRecordingsURL, {});
+
+
+const firebaseCMS = {
+  getUnmoderatedRecordings: getUnmoderatedRecordings,
+};
+
+export default firebaseCMS;
