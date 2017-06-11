a4369803eeeeb196730dbfb45019b6dc8ee5815a
Stewart <stew@rtsmith.io>
2017-06-11 13:42:26 -0400

    Code cleanup for sanity.

--- src/routes/giffer.js
+++ src/routes/giffer.js
@@ -1,3 +1,5 @@
+import GIF from 'gif.js';
+import CCapture from 'ccapture.js';
 import router from '../router';
 import Room from '../room';
 import Orb from '../orb';
@@ -7,123 +9,117 @@ import settings from '../settings';
 import hud from '../hud';
 import createTimeline from '../lib/timeline';
 import { waitRoomColor, recordRoomColor } from '../theme/colors';
-import { Vector3 } from '../lib/three';
+import { Vector3, WebGLRenderer, OrthographicCamera } from '../lib/three';
 import { sleep } from '../utils/async';
 import windowSize from '../utils/windowSize';
-// import capture from '../lib/ccaptureWrapper';
 
-import GIF from 'gif.js';
-import CCapture from 'ccapture.js';
-// import * as THREE from './three';
-
-//  Our capture settings:
-
-const width = 1024;
-const height = 1024;
-const duration = 16; // Unit is Seconds.
-const fps = 30;
-
-
-//  These are for my render tests.
-//  Can take them out for production.
-
-const filename = `${width.toString().padStart(4, '0')}x${height.toString().padStart(4, '0')}-${duration.toString().padStart(2, '0')}sec@${fps}fps-`;
-console.log(filename);
-let timeBegan;
-let timeEnded;
-
-
-//  We need our own renderer with dimensions
-//  equal to our target output GIF dimensions.
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setClearColor(0x000000);
-renderer.setPixelRatio(1); // window.devicePixelRatio
-renderer.setSize(width, height);
-renderer.sortObjects = false;
-
-const orthographicDistance = 4;
-const aspectRatio = width / height;
-const camera = new THREE.OrthographicCamera(
-  -orthographicDistance * aspectRatio,
-  orthographicDistance * aspectRatio,
-  orthographicDistance,
-  -orthographicDistance,
-  -100,
-  1000,
-);
-camera.position.set(0.06, 0.08, 0.08);
-camera.lookAt(new THREE.Vector3());
-
-
-//  BUG: Right now CCapture needs GIF to be a global variable.
-//  This is terrible. Need to patch that!!
-
-window.GIF = GIF;
-
-
-//  Now we can setup our CCapture instance.
-
-let capturer;
-setTimeout(() => {
-  capturer = new CCapture({
-    verbose: false, // false
-    display: true,
-    framerate: fps,
-    // motionBlurFrames: (960 / fps),
-    quality: 100,
-    format: 'webm', //  webm gif
-    workersPath: '../lib/',
-    // timeLimit: 10000 * duration,
-    // frameLimit: 10000 * duration * fps,
-    autoSaveTime: 0,
-
-
-    //  This is for my render tests.
-    //  Can take it out for production.
-
-    onProgress: function (p) {
-      console.log('Render is:', p * 100, '% complete.');
-      if (p === 0) {
-        timeBegan = new Date();
-        console.log('Render started at', timeBegan);
-      } else if (p === 1) {
-        timeEnded = new Date();
-        const timeDuration = new Date(timeEnded - timeBegan);
-        console.log(`${filename + timeDuration.getMinutes()}m${timeDuration.getSeconds().toString().padStart(2, '0')}s.gif`);
-      }
-    },
-  });
-});
-
-
-console.log('giffer is loaded');
-
-// TODO: replace with better recording:
-const TUTORIAL_RECORDING_URL = '1030266141029-b5ba6ff6.json';
-// one of my recents: 3/1027581876754-4dc51d61
-
-const { roomDepth, roomOffset } = settings;
-
-const getLineTransform = (x1, y1, x2, y2, margin) => {
-  const length = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) - margin;
-  const angle = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
-  return `translate(${x1}px, ${y1}px) rotate(${angle}deg) scaleX(${length / 100})`;
-};
 
 export default (req) => {
   //
-  //
-  //  GIF STUF!! ================================================================
-  console.log('giffing...');
-  // let id = parseInt(req.params.id, 10);
-  let id = req.params.id;
-  window.req = req;
-  if (!id) id = '1030266141029-b5ba6ff6'; // TUTORIAL_RECORDING_URL;
-  console.log('exporting this dance:', id);
-  window.danceId = id; //  temp for debugging!!
+
+  //  Our capture settings:
+
+  const width = 1024;
+  const height = 1024;
+  const duration = 16; // Unit is Seconds.
+  const fps = 30;
+
+
+  //  So what dance do we plan on rendering and exporting?
+  //  Default dance is the Tutorial recording.
+  //  http://localhost:3000/giffer/1030266141029-b5ba6ff6
+
+  const id = req.params.id ? req.params.id : '1030266141029-b5ba6ff6';
+
+
+  //  We need our own renderer with dimensions
+  //  equal to our target output GIF dimensions.
+
+  const renderer = new WebGLRenderer({ antialias: true });
+  renderer.setClearColor(0x000000);
+  renderer.setPixelRatio(1); // window.devicePixelRatio
+  renderer.setSize(width, height);
+  renderer.sortObjects = false;
+
+
+  //  We also need our own camera. One that has a fixed aspect ratio,
+  //  our own custom positions, and that doesn’t change on window resize!
+  //  ie. We can’t re-use viewer.camera.
+
+  const orthographicDistance = 4;
+  const aspectRatio = width / height;
+  const camera = new OrthographicCamera(
+    -orthographicDistance * aspectRatio,
+    orthographicDistance * aspectRatio,
+    orthographicDistance,
+    -orthographicDistance,
+    -100,
+    1000,
+  );
+  camera.position.set(0.06, 0.08, 0.08);
+  camera.lookAt(new Vector3());
+
+
+  //  BAD: Right now CCapture needs GIF to be a global variable.
+  //  This is terrible. Much sadness. Like tears in rain.
+
+  window.GIF = GIF;
+
+
+  //  These are for my render tests.
+  //  Can take them out for production.
+
+  const filename = `${width.toString().padStart(4, '0')}x${height.toString().padStart(4, '0')}-${duration.toString().padStart(2, '0')}sec@${fps}fps-`;
+  console.log(filename);
+  let timeBegan;
+  let timeEnded;
+
+
+  //  Now we can setup our CCapture instance.
+
+  let capturer;
   let gifFrame = 0;
   let gifProgress = 0;
+  setTimeout(() => {
+    capturer = new CCapture({
+      verbose: false,
+      display: true,
+      framerate: fps,
+      // motionBlurFrames: (960 / fps),
+      quality: 100,
+      format: 'webm', //  webm gif
+      workersPath: '../lib/',
+      // timeLimit: 10000 * duration,
+      // frameLimit: 10000 * duration * fps,
+      autoSaveTime: 0,
+
+
+      //  This is for my render tests.
+      //  Can take it out for production.
+
+      onProgress: function (p) {
+        console.log('Render is:', p * 100, '% complete.');
+        if (p === 0) {
+          timeBegan = new Date();
+          console.log('Render started at', timeBegan);
+        } else if (p === 1) {
+          timeEnded = new Date();
+          const timeDuration = new Date(timeEnded - timeBegan);
+          console.log(`${filename + timeDuration.getMinutes()}m${timeDuration.getSeconds().toString().padStart(2, '0')}s.gif`);
+        }
+      },
+    });
+  });
+
+
+  const { roomDepth, roomOffset } = settings;
+
+  const getLineTransform = (x1, y1, x2, y2, margin) => {
+    const length = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) - margin;
+    const angle = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+    return `translate(${x1}px, ${y1}px) rotate(${angle}deg) scaleX(${length / 100})`;
+  };
+
 
   let getLineTarget;
   let room;
@@ -142,25 +138,6 @@ export default (req) => {
 
     return TEMP_VECTOR;
   };
-
-
-  const gifferTimeline = createTimeline([
-    {
-      time: 0, // 0.5
-      callback: () => {
-        console.log('start GIF!!');
-        capturer.start();
-      },
-    },
-    {
-      time: 1, // 1.5
-      callback: () => {
-        console.log('end GIF!!');
-        capturer.stop();
-        capturer.save();
-      },
-    },
-  ]);
 
   const colorTimeline = createTimeline([
     {
@@ -184,17 +161,19 @@ export default (req) => {
     Room.clear();
 
 
-    gifProgress = gifFrame / (duration * fps);
-    // console.log(gifFrame, gifProgress);
-    const gifRoomProgress = gifProgress * 2;
-    window.gifFrame = gifFrame;
-    window.gifProgress = gifProgress;
+    //  What percentage of our GIF have we rendered?
 
-    window.audio = audio;
-    window.audioTime = audio.time;
+    gifProgress = gifFrame / (duration * fps);
+
+
+    //  Because our GIF timeline covers TWO rooms
+    //  worth of time we’re multiplying by 2.
+
+    const gifRoomProgress = gifProgress * 2;
+
+
     room.gotoTime(
-      // audio.time,
-      gifProgress * 2 * audio.loopDuration,
+      gifRoomProgress * audio.loopDuration,
       Math.max(
         state.minLayers,
         // Math.ceil((audio.totalProgress / 2) % 3)
@@ -225,16 +204,26 @@ export default (req) => {
       );
     }
 
-    //  GIF export! =====================================================================
-    // gifferTimeline.tick(audio.totalProgress);
-    gifferTimeline.tick(gifProgress);
-    // if (audio.totalProgress >= 0 && audio.totalProgress <= 2) {
-    if (gifFrame >= 0 && gifFrame <= duration * fps) {
-      renderer.render(viewer.scene, camera);
-      capturer.capture(renderer.domElement);
-    }
+
+    //  The giffer timeline controls when recording begins and ends.
+    //  We pass it a normalized progress float.
+    //  If we’re recording then we need to render and capture.
+
     if (audio.totalProgress >= 0) {
       gifFrame++;
+    }
+    if (gifFrame > 0 && gifFrame <= duration * fps) {
+      if (gifFrame === 1) {
+        console.log('Render GIF frames BEGIN');
+        capturer.start();
+      }
+      renderer.render(viewer.scene, camera);
+      capturer.capture(renderer.domElement);
+      if (gifFrame === duration * fps) {
+        console.log('Render GIF frames END');
+        capturer.stop();
+        capturer.save();
+      }
     }
   };
 
@@ -242,18 +231,6 @@ export default (req) => {
     state.lineOriginX = windowWidth / 2;
     state.lineOriginY = elements.tutorialText.offsetHeight * 1.2;
   };
-
-  // const handleKeyframe = ({ text, getPosition, layers }) => {
-  //   // just !text would return true on empty string, so:
-  //   if (text !== undefined) {
-  //     elements.tutorialText.innerHTML = text;
-  //   }
-  //   getLineTarget = getPosition;
-  //   elements.lineEl.style.opacity = getPosition ? 1 : 0;
-  //   if (layers !== undefined) {
-  //     state.minLayers = layers;
-  //   }
-  // };
 
   const component = {
     mount: async () => {
@@ -308,7 +285,7 @@ export default (req) => {
       hud.hideLoader();
 
       room = new Room({
-        url: `${id}.json`, // TUTORIAL_RECORDING_URL,
+        url: `${id}.json`,
         showHead: true,
         index: 0,
         recording: true,
@@ -344,7 +321,6 @@ export default (req) => {
       viewer.camera.zoom = 1;
       viewer.camera.updateProjectionMatrix();
       viewer.events.off('tick', tick);
-      // textTimeline.off('keyframe', handleKeyframe);
     },
   };
 
