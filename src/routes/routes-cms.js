aff939de996687f0a759d430c544344668bb6046
puckey <jonathan@puckey.studio>
2017-06-12 12:08:07 +0200

    turn on react dev tools in development mode

--- src/routes/routes-cms.js
+++ src/routes/routes-cms.js
@@ -2,6 +2,10 @@ const choose = require('./cms/choose').default;
 const inbox = require('./cms/inbox').default;
 const playback = require('./playback').default;
 
+if (process.env.NODE_ENV === 'development') {
+  require('preact/devtools');
+}
+
 export default {
   routes: {
     '/inbox/:recordingId?': inbox,
