4e58de7ff694e8b21bc427eb095a9476f2840779
puckey <jonathan@puckey.studio>
2017-06-23 13:52:33 +0200

    prevent unnecessary dom updates

--- src/components/Colophon/index.js
+++ src/components/Colophon/index.js
@@ -1,24 +1,30 @@
 /** @jsx h */
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import classNames from 'classnames';
 
 import './style.scss';
 import iconVR from './icon-vr-experiments.svg';
 import iconFriends from './icon-friends-of-google.svg';
 
-export default function Colophon({ className }) {
-  return (
-    <div className={classNames('colophon', className)}>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://webvrexperiments.com/"
-        dangerouslySetInnerHTML={{ __html: iconVR }}
-      />
-      <div className="colophon-divider" />
-      <span
-        dangerouslySetInnerHTML={{ __html: iconFriends }}
-      />
-    </div>
-  );
+export default class Colophon extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render({ className }) {
+    return (
+      <div className={classNames('colophon', className)}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://webvrexperiments.com/"
+          dangerouslySetInnerHTML={{ __html: iconVR }}
+        />
+        <div className="colophon-divider" />
+        <span
+          dangerouslySetInnerHTML={{ __html: iconFriends }}
+        />
+      </div>
+    );
+  }
 }
