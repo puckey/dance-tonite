e2ea4fab99eed7f06cf0c99b306b29ef87415044
David van Gelder de Neufville <_@neufv.website>
2017-06-26 12:34:58 +0200

    Make share links work, check if this browser should be able to render GIFs

--- src/containers/Submission/index.js
+++ src/containers/Submission/index.js
@@ -11,6 +11,7 @@ import GooglePlus from '../../components/GooglePlus';
 import ButtonItem from '../../components/ButtonItem';
 import audio from '../../audio';
 import viewer from '../../viewer';
+import feature from '../../utils/feature';
 import Container from '../../components/Container';
 
 export default class Submission extends Component {
@@ -24,6 +25,12 @@ export default class Submission extends Component {
       viewer.vrEffect.exitPresent();
     }
     viewer.switchCamera('orthographic');
+
+    this.shareURL = {
+      googlePlus: 'https://plus.google.com/share?url=',
+      twitter: 'https://twitter.com/intent/tweet?text=',
+      facebook: 'https://www.facebook.com/sharer/sharer.php?u=',
+    };
   }
 
   componentDidMount() {
@@ -35,8 +42,16 @@ export default class Submission extends Component {
     audio.fadeOut();
   }
 
+  share(service) {
+    window.open(
+      `${this.shareURL[service]}${this.deepLink}`,
+      '',
+      'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'
+    );
+  }
+
   render({ roomId, recordingId }) {
-    const deepLink = `https://tonite.dance/${roomId}/${recordingId}`;
+    this.deepLink = `https://tonite.dance/${roomId}/${recordingId}`;
     return (
       <Container>
         <Menu
@@ -51,14 +66,20 @@ export default class Submission extends Component {
         />
         <Align type="bottom-left">
           <p className="share-buttons">Share</p>
-          <Align type="inline" rows><GooglePlus /><Twitter /><Facebook /></Align>
+          <Align type="inline" rows>
+            <GooglePlus onClick={() => this.share('googlePlus')} />
+            <Twitter onClick={() => this.share('twitter')} />
+            <Facebook onClick={() => this.share('facebook')} />
+          </Align>
           <p className="share-buttons">
-            <a href={deepLink}>{deepLink}</a>
+            <a href={this.deepLink}>{this.deepLink}</a>
           </p>
         </Align>
-        <Align type="bottom-right">
-          <ButtonItem text navigate="/">Create animated GIF</ButtonItem>
-        </Align>
+        { feature.has6DOF &&
+          <Align type="bottom-right">
+            <ButtonItem text navigate="/">Create animated GIF</ButtonItem>
+          </Align>
+        }
       </Container>
     );
   }
