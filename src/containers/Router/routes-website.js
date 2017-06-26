b861e7ee65ce1deeda89547477db693667a47d25
David van Gelder de Neufville <_@neufv.website>
2017-06-26 15:18:31 +0200

    Remove submission route (is now handled by playback flow)

--- src/containers/Router/routes-website.js
+++ src/containers/Router/routes-website.js
@@ -1,9 +1,7 @@
 import RecordFlow from '../RecordFlow';
 import PlaybackFlow from '../PlaybackFlow';
-import Submission from '../Submission';
 
 export default {
   '/record/:roomId?/:hideHead?': RecordFlow,
   '/:roomId?/:id?': PlaybackFlow,
-  '/submission/:roomId?/:id?': Submission,
 };
