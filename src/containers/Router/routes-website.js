37b4d6a0bed1cfade5645c8c8f90e98cf1e9bde5
David van Gelder de Neufville <_@neufv.website>
2017-06-26 12:21:45 +0200

    Add buttons, deeplink, icons to Submission page

--- src/containers/Router/routes-website.js
+++ src/containers/Router/routes-website.js
@@ -5,5 +5,5 @@ import Submission from '../Submission';
 export default {
   '/record/:roomId?/:hideHead?': RecordFlow,
   '/:roomId?/:id?': Playback,
-  '/submission/:roomId?/:id?': Submission,
+  '/submission/:roomId?/:recordingId?': Submission,
 };
--- src/containers/Submission/index.js
+++ src/containers/Submission/index.js
@@ -1,10 +1,17 @@
 /** @jsx h */
 import { h, Component } from 'preact';
+import './style.scss';
 
 import Room from '../../components/Room';
+import Menu from '../../components/Menu';
 import Align from '../../components/Align';
+import Twitter from '../../components/Twitter';
+import Facebook from '../../components/Facebook';
+import GooglePlus from '../../components/GooglePlus';
+import ButtonItem from '../../components/ButtonItem';
 import audio from '../../audio';
 import viewer from '../../viewer';
+import Container from '../../components/Container';
 
 export default class Submission extends Component {
   constructor() {
@@ -29,14 +36,30 @@ export default class Submission extends Component {
   }
 
   render({ roomId, recordingId }) {
+    const deepLink = `https://tonite.dance/${roomId}/${recordingId}`;
     return (
-      <div>
+      <Container>
+        <Menu
+          about
+          mute
+          close
+        />
         <Room
           roomId={roomId}
           id={recordingId}
           orbs
         />
-      </div>
+        <Align type="bottom-left">
+          <p className="share-buttons">Share</p>
+          <Align type="inline" rows><GooglePlus /><Twitter /><Facebook /></Align>
+          <p className="share-buttons">
+            <a href={deepLink}>{deepLink}</a>
+          </p>
+        </Align>
+        <Align type="bottom-right">
+          <ButtonItem text navigate="/">Create animated GIF</ButtonItem>
+        </Align>
+      </Container>
     );
   }
 }
