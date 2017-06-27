d1efa2a05691691a726bbcede5a398f0a00e5ff0
puckey <jonathan@puckey.studio>
2017-06-27 14:21:06 +0200

    hide enter vr overlay from polyfill users  - closes #396

--- src/components/EnterVROverlay/index.js
+++ src/components/EnterVROverlay/index.js
@@ -1,9 +1,10 @@
 /** @jsx h */
 import { h } from 'preact';
 import './style.scss';
+import feature from '../../utils/feature';
 
 export default function EnterVROverlay() {
-  return (
+  return !feature.vrPolyfill && (
     <div className="vr-info-overlay mod-entering-vr">
       <div className="vr-info-overlay-text">
         Put on your VR headset
