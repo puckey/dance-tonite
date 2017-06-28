abbc3eb0da3055dac2a33ab10e2ed6815766fe26
Jeff Nusz <jnusz@google.com>
2017-06-28 15:55:54 -0700

    no polyfill on android

--- src/utils/feature.js
+++ src/utils/feature.js
@@ -97,7 +97,7 @@ const isAndroid = /android/i.test(userAgent);
 const isMobile = /android|ipad|iphone|iemobile/i.test(userAgent);
 const isTablet = (isAndroid && !/mobile/i.test(userAgent)) // https://stackoverflow.com/questions/5341637/how-do-detect-android-tablets-in-general-useragent
   || /ipad/i.test(userAgent);
-const vrPolyfill = isMobile && !isTablet && (navigator.getVRDisplays === undefined);
+const vrPolyfill = isMobile && !isTablet && !isAndroid && (navigator.getVRDisplays === undefined);
 
 
 const feature = {
