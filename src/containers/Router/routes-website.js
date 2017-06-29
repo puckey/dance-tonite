11a605c0075cab8d32c18e47cdfddd2259fe7ef5
puckey <jonathan@puckey.studio>
2017-06-29 16:47:04 +0200

    /gallery/

--- src/containers/Router/routes-website.js
+++ src/containers/Router/routes-website.js
@@ -1,7 +1,9 @@
 import RecordFlow from '../RecordFlow';
 import PlaybackFlow from '../PlaybackFlow';
+import Gallery from '../Gallery';
 
 export default {
+  '/gallery/:id?': Gallery,
   '/record/:roomId?/:hideHead?': RecordFlow,
   '/:roomId?/:id?': PlaybackFlow,
 };
