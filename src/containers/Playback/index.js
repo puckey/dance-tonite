4d5edb4b24ee319f3f36db3cc78f9f11bdf5def3
puckey <jonathan@puckey.studio>
2017-07-05 11:52:42 +0200

    Press instead of click

--- src/containers/Playback/index.js
+++ src/containers/Playback/index.js
@@ -176,7 +176,7 @@ export default class Playback extends Component {
               ? (
                 <Overlay opaque>
                   <a onClick={onGotoSubmission}>
-                    <span>Click here to continue</span>
+                    <span>Press here to continue</span>
                   </a>
                 </Overlay>
               )
