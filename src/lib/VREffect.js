45a5398e8912c53ff6484c59c7e31a0d1463b5b0
Jeff Nusz <jnusz@google.com>
2017-07-06 16:22:29 -0700

    put dynamic res back into VREffect

--- src/lib/VREffect.js
+++ src/lib/VREffect.js
@@ -111,6 +111,7 @@ module.exports = function( THREE ){
 		var canvas = renderer.domElement;
 		var defaultLeftBounds = [ 0.0, 0.0, 0.5, 1.0 ];
 		var defaultRightBounds = [ 0.5, 0.0, 0.5, 1.0 ];
+		var VRResolutionRatio = 1.0;
 
 		function onVRDisplayPresentChange() {
 
@@ -134,6 +135,7 @@ module.exports = function( THREE ){
 					if (rendererPixelRatio !== 1) renderer.setPixelRatio(1);
 					renderer.setSize(eyeWidth * 2, eyeHeight, false);
 
+					scope.setVRResolutionRatio(VRResolutionRatio)
 				}
 
 			} else if ( wasPresenting ) {
@@ -142,10 +144,51 @@ module.exports = function( THREE ){
 				renderer.setSize( rendererSize.width, rendererSize.height, rendererUpdateStyle );
 
 			}
-
+			
+			//renderer.domElement.style.width = rendererSize.width + "px";
+			//renderer.domElement.style.height = rendererSize.height + "px";
 		}
 
 		window.addEventListener( 'vrdisplaypresentchange', onVRDisplayPresentChange, false );
+
+		// Set the resolution draw and presented in VR. This ratio only affects the area of the
+		// canvas we render to and submit to VRDisplay (no canvas resizing required), so 
+		// this function is fast enough to call every frame
+		this.setVRResolutionRatio = function (ratio) {
+			VRResolutionRatio = THREE.Math.clamp(ratio, 0, 1.0);
+
+			if ( vrDisplay !== undefined && vrDisplay.isPresenting ) {
+
+				// since we're already presenting, this doesn't need to be initiated by user interaction
+				requestPresentToVRDisplay();
+
+		        if (vrDisplay.capabilities.hasExternalDisplay) {
+
+		          // scale mirrored content up to fill the screen (even if we're just drawing to 
+		          // a small part of it)
+		          canvas.style.width = (1.0/VRResolutionRatio) * 100 + "%";
+		          canvas.style.height = (1.0/VRResolutionRatio) * 100 + "%";
+		        }
+			}
+
+		}
+
+		function requestPresentToVRDisplay() {
+			var eyeParamsL = vrDisplay.getEyeParameters( 'left' );
+
+			// render size of each divided by total canvas size 
+			var boundsWidth = Math.floor(eyeParamsL.renderWidth * VRResolutionRatio) / canvas.width;
+			var boundsHeight = Math.floor(eyeParamsL.renderHeight * VRResolutionRatio) / canvas.height;
+
+			console.log("VR Effect VRResolutionRatio: ", VRResolutionRatio, " ", eyeParamsL.renderWidth * VRResolutionRatio, "x", eyeParamsL.renderHeight * VRResolutionRatio);
+
+			return vrDisplay.requestPresent([{
+	         	source: canvas,
+	      	 	leftBounds: [0.0, 0.0, boundsWidth, boundsHeight],
+	      	 	rightBounds: [boundsWidth, 0.0, boundsWidth, boundsHeight]
+	        }]);
+		}
+
 
 		this.setFullScreen = function ( boolean ) {
 
@@ -167,7 +210,7 @@ module.exports = function( THREE ){
 
 				if ( boolean ) {
 
-					resolve( vrDisplay.requestPresent( [ { source: canvas } ] ) );
+					resolve( requestPresentToVRDisplay() );
 
 				} else {
 					if (!vrDisplay.isPresenting) {
@@ -356,9 +399,9 @@ module.exports = function( THREE ){
 
 				// render left eye
 				if ( renderTarget ) {
-
-					renderTarget.viewport.set( renderRectL.x, renderRectL.y, renderRectL.width, renderRectL.height );
-					renderTarget.scissor.set( renderRectL.x, renderRectL.y, renderRectL.width, renderRectL.height );
+					
+					renderer.setViewport( renderRectL.x, renderRectL.y + (size.height - renderRectL.height), renderRectL.width, renderRectL.height );
+					renderer.setScissor( renderRectL.x, renderRectL.y + (size.height - renderRectL.height), renderRectL.width, renderRectL.height );
 
 				} else {
 
@@ -376,8 +419,9 @@ module.exports = function( THREE ){
 
 				} else {
 
-					renderer.setViewport( renderRectR.x, renderRectR.y, renderRectR.width, renderRectR.height );
-					renderer.setScissor( renderRectR.x, renderRectR.y, renderRectR.width, renderRectR.height );
+					renderer.setViewport( renderRectR.x, renderRectR.y + (size.height - renderRectR.height), renderRectR.width, renderRectR.height );
+					renderer.setScissor( renderRectR.x, renderRectR.y + (size.height - renderRectR.height), renderRectR.width, renderRectR.height );
+
 
 				}
 				renderer.render( scene, cameraR, renderTarget, forceClear );
