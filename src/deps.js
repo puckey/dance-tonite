49bc0ea62d74e03552c5a4f15b1db4a242e60b09
Flux <flux.blackcat@gmail.com>
2017-07-13 15:32:19 -0700

    forcing sdf to be a dependency when vr is available

--- src/deps.js
+++ src/deps.js
@@ -2,11 +2,22 @@ import feature from './utils/feature';
 
 const packs = [
   {
-    test: () => feature.has6DOF,
+    test: () => feature.hasWebVR,
     prepare: () => (
       new Promise((resolve) => {
         require.ensure([], function (require) {
           deps.SDFText = require('./sdftext');
+          // console.log(SDFText);
+          resolve();
+        });
+      })
+    ),
+  },
+  {
+    test: () => feature.has6DOF,
+    prepare: () => (
+      new Promise((resolve) => {
+        require.ensure([], function (require) {
           deps.controllers = require('./controllers').default;
           deps.GIF = require('./lib/gif');
           resolve();
