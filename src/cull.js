8c1ca3ffb63ac641ecd83120dc42ebea9afe1d42
puckey <jonathan@puckey.studio>
2017-07-13 12:34:03 +0200

    clean

--- src/cull.js
+++ src/cull.js
@@ -4,11 +4,10 @@ import viewer from './viewer';
 let prevTime;
 let frames = 0;
 let fps = 60;
-let lastFps = 60;
-let steadyCount = 0;
 let hidden = false;
 let cullDistance = settings.cullDistance;
 let fogNear = settings.fogNear;
+const logging = false;
 
 document.addEventListener('visibilitychange', () => {
   hidden = document.hidden;
@@ -25,35 +24,26 @@ export default (interval = 3000) => {
   if (prevTime == null) prevTime = time;
   if (time > prevTime + interval) {
     fps = (frames * 1000) / (time - prevTime);
+    // Change FPS to multiples of 50 in order to ignore slight fluctuations:
     fps = Math.min(55, Math.round(fps * 0.2) * 5);
     frames = 0;
     prevTime = time;
-
-    if (fps === lastFps) {
-      steadyCount++;
-    } else {
-      steadyCount = 0;
-    }
-    lastFps = fps;
     const lastCullDistance = cullDistance;
-    if (fps < 52) {
-      cullDistance = Math.max(
+    cullDistance = fps <= 50
+      ? Math.max(
         settings.minCullDistance,
         cullDistance - settings.roomDepth
-      );
-    } else {
-      cullDistance = Math.min(
+      )
+      : Math.min(
         settings.maxCullDistance,
         cullDistance + settings.roomDepth
       );
-    }
     fogNear = settings.cullDistance - settings.roomDepth;
-    if (lastCullDistance !== cullDistance) {
-      console.log('Changed cull distance to', cullDistance);
+    if (logging && lastCullDistance !== cullDistance) {
+      console.log(`${lastCullDistance > cullDistance ? 'Lowering' : 'Upping'} cull distance to`, cullDistance);
     }
   }
   settings.cullDistance = settings.cullDistance * 0.95 + cullDistance * 0.05;
   settings.fogNear = settings.fogNear * 0.95 + fogNear * 0.05;
   viewer.fog.near = settings.fogNear;
-  return fps;
 };
